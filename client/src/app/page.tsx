5c88e6f3857324d033adbd2e9f15ab90390c30e2
diego <diego.roque0309@gmail.com>
2025-05-06 21:17:18 -0600

    title change

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
       <>
           <Head>
-              <title>Diego Roque Portafolio</title>
+              <title>Portafolio Diego Roque </title>
           </Head>
     <div className="bg-black">
 
